e260c04ef72602ca9035f1b0636d48e255db564a
MaciekCelinski <mac.celinski@gmail.com>
2022-07-26 22:41:30 +0200

    Lecture 26 - Before dynamic and nest routes

---
+++ components/restaurant/Row.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="row" v-if="isHeader">
+    <h4 class="header">Rank</h4>
+    <h4 class="header">Chain</h4>
+  </div>
+  <div v-if="!isHeader" class="row" :style="index % 2 !== 0 ? { background: 'rgba(128,128,128,0.15)' } : null">
+    <h4 class="header rank">{{ restaurant.id }}</h4>
+    <a :href="`/restaurants/${restaurant.name}`" class="header link">{{ restaurant.name }}</a>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+// defineProps({
+//   restaurant: Object,
+//   index: Number,
+//   isHeader: Boolean
+// })
+
+interface RowProps {
+  restaurant?: {
+    id: number,
+    name: string,
+  },
+  index?: number,
+  isHeader: boolean
+}
+
+const props = defineProps<RowProps>()
+
+</script>
+
+<style scoped>
+.row {
+  border-top: 1px solid rgba(128, 128, 128, 0.336);
+  display: flex;
+  align-items: center;
+}
+
+.header {
+  font-size: 1.25rem;
+  color: rgb(10, 31, 148);
+  margin: 0;
+  margin-right: 2rem;
+  padding: 0.5rem 1rem;
+  font-weight: 700;
+  width: 4rem;
+}
+
+.rank {
+  color: black;
+}
+
+.link {
+  text-decoration: none;
+  color: rgb(228, 157, 27);
+  width: auto;
+  font-weight: 500;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Nav />
+    <div class="container">
+      <h1>Welcome to Restaurantly</h1>
+      <a href="/restaurants">Go to restaurants</a>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.container {
+  text-align: center;
+  margin-top: 3rem;
+}
+</style>
